dc0b4166c38ce8f343db5dba92cb1755ea579f26
Joel Ye <jye5@stuy.edu>
2016-09-16 22:25:52 -0400

    ugh

--- p10/p10primeSum.c
+++ p10/p10primeSum.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int primes[1000000];
+//store all the primes
+
+//calculate the next prime, add the list
+//index is last prime known so far
+int nextPrime(int index){
+  int n = primes[index];
+  n++;
+  int ctr;
+  int isPrime = 0;
+  while (!isPrime){
+    for (ctr = 0; ctr <= index; ctr++){
+      if (n % primes[ctr] == 0) break;
+      if (ctr == index) {
+	isPrime = 1;
+	primes[index+1] = n;
+      }
+    }
+    n++;
+  }
+}
+
+long sumArr(int a[], int cap){
+  int ctr;
+  long sum = 0;
+  for (ctr = 0; ctr < cap; ctr++){
+    sum += a[ctr];
+  }
+  return sum;
+}
+
 int main(){
+  primes[0] = 2;
+  int i = 0;
+  while (primes[i] < 2000000){
+    nextPrime(i);
+    i++;
+  }
+  printf("%d", sumArr(primes, i));
   return 0;
 }
